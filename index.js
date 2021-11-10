72cde2dfb9895874a96e0f3dbcd8647abf465be9
StasGil <stanislav.gill@jadexconsulting.com>
2021-11-10 12:05:18 +0200

    Commit for testing of  heroku

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const toDoRoute = require("./todo");
+
+const PORT = process.env.PORT || 4000;
+
+const mongoString =
+  "mongodb+srv://stan:stan12@cluster0.5wrpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const app = express();
+app.use(toDoRoute);
+
+async function start() {
+  try {
+    await mongoose.connect(mongoString, {
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => {
+      console.log("Server has been started at port ", PORT);
+    });
+  } catch (e) {
+    console.log("ERROR", e);
+  }
+}
+
+start();
